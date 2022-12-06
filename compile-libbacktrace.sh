b46c364a6dcebd97ea7be19c827a45ec1a34f4f8
Xiao Zeng <zengxiao@eswincomputing.com>
2022-12-06 16:38:00 +0800

    compile-libbacktrace.sh

---
+++ compile-libbacktrace.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+set -ex
+
+mkdir -p build && cd build
+
+if [[ -f Makefile ]];then
+  make distclean
+  rm -rf Makefile
+fi
+
+../configure --prefix=`pwd`/../install CFLAGS="-g3 -ggdb3 -gvariable-location-views -gdwarf-4 -grecord-gcc-switches -O1 -Werror"
+
+make -j1 2>&1 | tee make-j1.log
+# make -d --trace -j1 2>&1 | tee make-j1-debug.log
+# make -j`nproc` 2>&1 | tee make-j`nproc`.log
+# make -d --trace -j`nproc` 2>&1 | tee make-j`nproc`-debug.log
+make install
+echo $?
