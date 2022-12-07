f01813c86a5dfc70652b10ed286eee2c51c1109f
Xiao Zeng <zengxiao@eswincomputing.com>
2022-12-07 17:50:18 +0800

    compile-libbacktrace.sh

--- compile-libbacktrace.sh
+++ compile-libbacktrace.sh
@@ -15,4 +15,9 @@ make -j1 2>&1 | tee make-j1.log
 # make -j`nproc` 2>&1 | tee make-j`nproc`.log
 # make -d --trace -j`nproc` 2>&1 | tee make-j`nproc`-debug.log
 make install
+
+# test
+make test_elf_64
+../test-driver --test-name `pwd`/x.log --log-file `pwd`/y.log --trs-file `pwd`/z.log `pwd`/test_elf_64
+
 echo $?
